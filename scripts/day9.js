3aaafaa5849b06c48314302413ed4b24fd7c42db
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-11-03 17:05:46 +0200

    including javascript in hour HTML

---
+++ scripts/day9.js
@@ -0,0 +1,11 @@
+// This is a comment in Javascript it does nothing for now
+//console.log is used for debugging purposes
+//it prints the content inside the parentheses to the console in your browser
+//this console is available in developer tools (F12 in most browsers)
+console.log("Hello, welcome to Day 9 of Web Development!");
+
+//let's change background color of header_id element
+document.getElementById("header_id").style.backgroundColor = "lightblue";
+console.log("Changed background color of header_id to lightblue");
+
+console.log("End of day9.js");
