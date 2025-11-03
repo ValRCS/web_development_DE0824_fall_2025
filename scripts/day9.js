4df5987c82c983bbf487170bd410b93418cb969e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-11-03 17:30:21 +0200

    added some more Javascript action

--- scripts/day9.js
+++ scripts/day9.js
@@ -3,6 +3,8 @@
 //it prints the content inside the parentheses to the console in your browser
 //this console is available in developer tools (F12 in most browsers)
 console.log("Hello, welcome to Day 9 of Web Development!");
+//typically console.log is used for debugging purposes
+//it is turned off in final production code usually
 
 //let's change background color of header_id element
 document.getElementById("header_id").style.backgroundColor = "lightblue";
@@ -14,8 +16,13 @@ console.log("End of day9.js");
 //so I've created an instruction to get the element by its ID
 //note I use const because I don't plan to change this variable later
 const outputElement = document.getElementById("output_id");
+//so instead of writing document.getElementById("output_id") every time
+//I can just use outputElement variable
 
 //so rule of thumb we use const unless we plan to change the variable later
+//when we need something that changes we use let keyword
+
+//in old days we only had var keyword but it had some issues so we avoid using it now
 
 //let's print some text to that element
 outputElement.textContent = "This text is added by Javascript code in day9.js file.";
@@ -48,6 +55,12 @@ outputElement.textContent += `\nThe exponentiation of ${a} to the power of ${b} 
 console.log("Added all mathematical operations to output_id element");
 
 //let's add current date and time to footer_id element
+//so again typically we find the element first
+//we store it in a variable for easier access
+//again here we use a const because we don't plan to change this variable later
 const footerElement = document.getElementById("footer_text");
 const currentDate = new Date();
 footerElement.textContent += `\nCurrent date and time: ${currentDate.toString()}`;
+console.log("Added current date and time to footer_text element");
+
+//again console.log is not required just using it for debugging and demonstration purposes
