b975ccaae5496f65c7198f4a76867f3d3ca1b825
Valdis Saulespurens <valdis.s.coding@gmail.com>
2025-12-08 16:42:13 +0200

    Add Day 14 content: Introduce Javascript Objects, JSON, and Event Target Identification

---
+++ scripts/day14.js
@@ -0,0 +1,5 @@
+//Today we will tak about Javascript Objects, JSON, Event Target Identification
+console.log("Day 14 - Javascript Objects, JSON, Event Target Identification");
+
+//Javascript objects are collections of key-value pairs
+//they are similar to Python dictionaries
